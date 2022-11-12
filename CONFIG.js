8e06921e71ecb07fac44ad6476d36c05690d4a10
congnt0610 <98198394+congnt0610@users.noreply.github.com>
2022-11-12 20:47:58 +0700

    upload

---
+++ CONFIG.js
@@ -0,0 +1,8 @@
+const CONFIGDATA = {
+    titleHeader: "Em bé làm em bé của anh nhớ",
+    descriptionHeader: "Tình iu to đùng của anh",
+    buttonYes: "Bé đồng ý",
+    buttonNo: "Em bé khum 🥲",
+    titleModar: "Iu em bé nhiều",
+    descriptionModar: 'Anh khum biết ghi gì cả ^^ </br>Chỉ biết nói iu em bé của anh thoii</br> "Iu em bé </br> Iu bé Linh" </br> World cup năm nay đi xem cùng anh nha em bé',
+};
